16f821cfe4c2668d1840aa75ba6082da2976e6c4
thiago2santos <thisanti@hotmail.com>
2024-04-25 09:24:09 -0300

    Usando o mapper

--- src/main/java/guru/springframework/microserviceswithspringcloud/usecase/impl/CustomerUsecaseImpl.java
+++ src/main/java/guru/springframework/microserviceswithspringcloud/usecase/impl/CustomerUsecaseImpl.java
@@ -1,26 +1,39 @@
 package guru.springframework.microserviceswithspringcloud.usecase.impl;
 
 import guru.springframework.microserviceswithspringcloud.entrypoint.CustomerDTO;
+import guru.springframework.microserviceswithspringcloud.model.Customer;
 import guru.springframework.microserviceswithspringcloud.usecase.CustomerCrudOperations;
+import guru.springframework.microserviceswithspringcloud.utils.mappers.CustomerMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Path;
 import java.util.UUID;
 
 @Slf4j
 @Service
 public class CustomerUsecaseImpl implements CustomerCrudOperations {
+
+    @Autowired
+    private final CustomerMapper customerMapper;
+
+    public CustomerUsecaseImpl(CustomerMapper customerMapper) {
+        this.customerMapper = customerMapper;
+    }
+
     @Override
     public CustomerDTO getById(UUID uuid) {
-        CustomerDTO customer = new CustomerDTO(uuid, "Thiago dos Santos");
-        return customer;
+        Customer customer = new Customer(uuid, "Thiago dos Santos");
+        CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+        return customerDTO;
     }
 
     @Override
     public CustomerDTO create(CustomerDTO customerDTO) {
-        CustomerDTO customer = customerDTO;
+        Customer customer = customerMapper.customerDTOToCustomer(customerDTO);
         customer.setUuid(UUID.randomUUID());
-        return customer;
+        return customerMapper.customerToCustomerDTO(customer);
     }
 
     @Override
